3861f8c1fd377c2cb778fef6f29df6ebad52fce4
Gary Giambatista <gary.giambatista@gmail.com>
2022-11-22 23:09:15 -0500

    installed stack-navigator and configured basic home and login screens with it.

---
+++ StackNavigator.js
@@ -0,0 +1,31 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import * as React from "react";
+import HomeScreen from "./screens/HomeScreen";
+import LoginScreen from "./screens/LoginScreen";
+
+const Stack = createNativeStackNavigator();
+
+const StackNavigator = () => {
+	const user = true;
+
+	return (
+		<Stack.Navigator
+			screenOptions={{
+				headerShown: false,
+			}}
+		>
+			{user ? (
+				<>
+					<Stack.Group>
+						<Stack.Screen name="Home" component={HomeScreen} />
+					</Stack.Group>
+				</>
+			) : (
+				<Stack.Screen name="Login" component={LoginScreen} />
+			)}
+		</Stack.Navigator>
+	);
+};
+
+export default StackNavigator;
